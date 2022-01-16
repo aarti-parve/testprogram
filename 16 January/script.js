2c99fc6fd6afffa89301368e3d0215e6453801b2
aarti-parve <90634943+aarti-parve@users.noreply.github.com>
2022-01-16 19:07:00 +0530

    Add files via upload

---
+++ 16 January/script.js
@@ -0,0 +1,37 @@
+let lon;
+let lat;
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+const kelvin = 273;
+
+
+
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            const api = "6d055e39ee237af35ca066f35474e9df";
+            const result = await superface.getUseCase('Geocode')
+                .perform({ "query": "kanpur" });
+            // Calling the API
+            fetch(base)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    temperature.textContent =
+                        Math.floor(data.main.temp - kelvin) + "°C";
+                    summary.textContent = data.weather[0].description;
+                    loc.textContent = data.name + "," + data.sys.country;
+                    let icon1 = data.weather[0].icon;
+
+
+                });
+        });
+    }
+});
